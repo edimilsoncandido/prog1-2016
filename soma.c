1b7ecee7428afada51f73b6587630f3881c5a931
Edimilson Maia Candido <edimilson.unilasalle@gmail.com>
2016-04-14 13:34:54 +0000

    soma

---
+++ soma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    int soma;
+    
+    num1 = 9;
+    num2 = 10;
+    
+    soma = num1 + num2;
+    
+    printf("\nsoma = %d", soma);
+    
+    return 0;
+}
